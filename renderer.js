e5429f3eeb80e8840ed89405a5a6865d5ef39556
Terrazero <terrahen@web.de>
2016-06-01 16:12:03 +0200

    change styling

--- renderer.js
+++ renderer.js
@@ -4,9 +4,3 @@ var render = sys.module('render');
 var $ = sys.src('js', 'jquery-2.2.4.min.js');
 var Node = sys.use('Node');
 
-var item = new Node();
-
-item.fields.title = 'Test';
-item.fields.body = 'Test Body';
-
-$('#content').append(item.view('full'));
