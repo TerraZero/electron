17fdcb97f22d377f68a0e9aff26948b0bf2b4ed3
Terrazero <terrahen@web.de>
2016-06-01 17:06:45 +0200

    add squel

--- modules/db.js
+++ modules/db.js
@@ -1,21 +1,27 @@
 'use strict';
 
 var mysql      = require('mysql');
+var squel = sys.module('squel');
+
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : '',
-  database : 'cducsu',
+  database : 'electron',
 });
 
 connection.connect();
 
-connection.query('SELECT s.name AS name, s.weight FROM system AS s WHERE s.weight <> 0', function(err, rows, fields) {
+var q = squel.insert()
+  .into('user')
+  .set('uid', 5)
+  .set('name', 'TerraZero');
+
+connection.query(q.toString(), function(err, rows, fields) {
   if (err) throw err;
 
-  for (var index in rows) {
-    console.log(rows[index].name, rows[index].weight);
-  }
+  console.log(rows);
+  console.log(fields);
 });
 
 connection.end();
--- renderer.js
+++ renderer.js
@@ -3,3 +3,4 @@
 var $ = sys.src('js', 'jquery-2.2.4.min.js');
 var Node = sys.use('Node');
 
+sys.module('db');
