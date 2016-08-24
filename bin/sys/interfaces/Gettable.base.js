939203f0f0f60d5ad30d5b96a99bbb9f29037b94
TerraZero <p.sagitza@loom-berlin.com>
2016-08-24 12:07:40 +0200

    add gattable

--- bin/sys/interfaces/Gettable.base.js
+++ bin/sys/interfaces/Gettable.base.js
@@ -2,24 +2,41 @@
 
 module.exports = class Gettable {
 
-  getting(key = null) {
+  getting(key = null, fallback = null) {
+    return this.gettingInfo(key, fallback).object;
+  }
+
+  gettingRoute(key = null) {
+    return this.gettingInfo(key).path.join('.');
+  }
+
+  gettingInfo(key = null, fallback = null) {
     if (key === null) return this;
 
+    var result = {
+      path: [],
+      object: this,
+    };
     var keys = key.split('.');
     var object = this;
 
     for (var i in keys) {
-      if (TOOLS.isArray(object)) {
+      if (TOOLS.isArray(object) && TOOLS.isInt(keys[i])) {
         object = object[parseInt(keys[i])];
       } else if (TOOLS.isFunction(object[keys[i]])) {
         object = object[keys[i]]();
       } else {
         object = object[keys[i]];
       }
-    }
-    return object;
-  }
 
-  SYS.use('bin/commad')
+      if (object === undefined) {
+        object = fallback;
+        break;
+      }
+      result.path.push(keys[i]);
+    }
+    result.object = object;
+    return result;
+  }
 
 };
